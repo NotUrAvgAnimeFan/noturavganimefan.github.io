8e348b88bac26f8d7743a264d65722012f31fd38
Ricardo Morales Gonzalez <moralesgonzalezyb12@gmail.com>
2025-08-09 15:50:54 -0400

    some progress

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,8 +4,10 @@ import About from './about.tsx';
 export default function Home() {
     return (
     <div className="min-h-screen bg-gray-600 px-8 font-[Rationale]">
-        <Navbar />
-        <About />
+        <div className='lg:max-w-1/2 mx-auto'>
+            <Navbar />
+            <About />
+        </div>
     </div>
     )
     
