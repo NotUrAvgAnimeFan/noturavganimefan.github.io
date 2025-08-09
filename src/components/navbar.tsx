955268e6ff9f320f0ccbb19094d47c1962f5c904
Ricardo Morales Gonzalez <moralesgonzalezyb12@gmail.com>
2025-08-09 00:38:21 -0400

    added images and starter

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Navmenu from './navmenu.tsx';
 export default function Navbar() {
     return (
     <div className="flex justify-evenly items-center pt-5">
-        <h1 className="text-3xl font-bold font-[Rationale] text-center">
+        <h1 className="text-3xl font-bold text-center text-white">
             Ricardo Manuel Morales Gonzalez
         </h1>
         <Navmenu />
