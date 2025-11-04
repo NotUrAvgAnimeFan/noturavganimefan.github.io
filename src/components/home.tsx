eb7d08b6e4bde35d76a1c6d1f1d7c541e6600a52
Ricardo <moralesgonzalezyb12@gmail.com>
2025-11-03 23:49:08 -0500

    Updated about Section

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -37,26 +37,16 @@ export default function Home() {
                 <ExampleComponent />
             </div>
             <p className="text-xs sm:text-sm pt-2 text-left">
-                I am a software engineer and web developer with a passion for creating beautiful and functional websites and applications. I am a graduate from The Johns Hopkisn University with a bachelor's in Computer Science and a minor in Entrepreneurship and Management. In addition to working with modern frameworks like React and Node.js, I have been working with AI tools and LLMs over the past year at Ringer Sciences creating insightful chatbots that enhance and breakdown complex data analytics into digestable insights for users of all levels.
+                I am a Software Engineer with a B.S. in Computer Science from Johns Hopkins University and a background in full-stack web development. My expertise lies in building functional, scalable applications using React and Node.js. Over the past year, I've applied these skills in the AI space at Ringer Science with Python as my main development language, engineering conversational chatbots that distill complex data into digestible insights, empowering users to make data-driven decisions.
             </p>
             <h2 className="text-base sm:text-lg pt-4 font-bold">Projects</h2>
             <div className='flex flex-col items-center'>
                 <div className='text-xs sm:text-sm text-left sm:max-w-3xl'>
                 <ul className="list-disc list-inside pt-2">
                     <li>
-                        <a href="https://etch-a-sketch-nu-five.vercel.app/" target='_blank' className="text-blue-400 hover:underline">
-                            Etch-A-Sketch
-                        </a>: Draw what your heart desires on this near infinite canvas.
-                    </li>
-                    <li>
-                        <a href="https://noturavganimefan.github.io/sleepy-times/" target='_blank' className="text-blue-400 hover:underline">
-                            Sleepy Times
-                        </a>: Want to know when to fall asleep to wake up feeling amazing? Use this calculator to know when you should fall asleep based on sleep cycles.
-                    </li>
-                    <li>
-                        <a href="https://noturavganimefan.github.io/sticky-notes/" target='_blank' className="text-blue-400 hover:underline">
-                            Sticky Notes
-                        </a>: A simple and intuitive sticky notes application built with React.
+                        <a href="https://song-info-one.vercel.app/" target='_blank' className="text-blue-400 hover:underline">
+                            Spotify Song Info
+                        </a>: Get some of the metadata correspondig to your favorite Spotify songs. This app uses the Spotify API to fetch song information.
                     </li>
                     <li>
                         <a href="https://noturavganimefan.github.io/manga-creation/" target='_blank' className="text-blue-400 hover:underline">
@@ -64,18 +54,32 @@ export default function Home() {
                         </a>: Create your own manga cover using AI. Simply provide a title and a description and let AI do the rest!
                     </li>
                     <li>
-                        <a href="https://song-info-one.vercel.app/" target='_blank' className="text-blue-400 hover:underline">
-                            Spotify Song Info
-                        </a>: Get some of the metadata correspondig to your favorite Spotify songs. This app uses the Spotify API to fetch song information.
+                        <a href="https://noturavganimefan.github.io/sticky-notes/" target='_blank' className="text-blue-400 hover:underline">
+                            Sticky Notes
+                        </a>: A simple and intuitive sticky notes application built with React.
                     </li>
+                    <li>
+                        <a href="https://noturavganimefan.github.io/sleepy-times/" target='_blank' className="text-blue-400 hover:underline">
+                            Sleepy Times
+                        </a>: Want to know when to fall asleep to wake up feeling amazing? Use this calculator to know when you should fall asleep based on sleep cycles.
+                    </li>
+                    <li>
+                        <a href="https://etch-a-sketch-nu-five.vercel.app/" target='_blank' className="text-blue-400 hover:underline">
+                            Etch-A-Sketch
+                        </a>: Draw what your heart desires on this near infinite canvas.
+                    </li>
+
+
+
+
                 </ul>
             </div>
             </div>
             <h2 className="text-base sm:text-lg pt-4 font-bold">Resume</h2>
             <Resume />
-            <h2 className="text-base sm:text-lg pt-4 font-bold">About Me</h2>
-        </div>        
+            {/* <h2 className="text-base sm:text-lg pt-4 font-bold">About Me</h2> */}
+        </div>
     </div>
     )
-    
+
 }
